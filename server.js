2bd032f48ba7fa94196b0dd13151a62c5e78b246
Joseph Munyao <e28p64@gmail.com>
2023-06-08 16:45:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,46 +7,60 @@ const PORT = 5500;
 
 // Serve static files from the "views" directory
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'data')));
 
 // Serve the about page
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 
 // Serve the add post page
 app.get('/posts/add', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/addPost.html'));
+  res.sendFile(path.join(__dirname, 'views/addPost.html'));
 });
 
 // API endpoint to retrieve categories
-app.get('/data/categories', (req, res) => {
-  const categoriesPath = path.join(__dirname, '/data/categories.json');
+app.get('/categories', (req, res) => {
+  const categoriesPath = path.join(__dirname, 'data/categories.json');
   fs.readFile(categoriesPath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send('Internal Server Error');
     } else {
-      res.json(JSON.parse(data));
+      const categories = JSON.parse(data);
+      const categoryList = categories.map(category => `<li>${category.category}</li>`).join('');
+      const html = generatePage(`<ul>${categoryList}</ul>`);
+      res.send(html);
     }
   });
 });
 
 // API endpoint to retrieve posts
-app.get('/data/posts', (req, res) => {
-  const postsPath = path.join(__dirname, '/data/posts.json');
+app.get('/posts', (req, res) => {
+  const postsPath = path.join(__dirname, 'data/posts.json');
   fs.readFile(postsPath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send('Internal Server Error');
     } else {
-      res.json(JSON.parse(data));
+      const posts = JSON.parse(data);
+      const postList = posts.map(post => `
+        <li class="list-group-item">
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+          <p>Posted on: ${post.postDate}</p>
+          <p>Category: ${post.category}</p>
+        </li>
+      `).join('');
+      const html = generatePage(`<ul class="list-group">${postList}</ul>`);
+      res.send(html);
     }
   });
 });
 
 // Handle all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 
 // Start the server
